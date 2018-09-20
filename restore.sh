efd57f246edb6cb88e95781e1182a344fd711e75
ghlin <2012.2.9.ghl@gmail.com>
2018-09-20 15:07:48 +0800

    restore script

--- restore.sh
+++ restore.sh
@@ -1,19 +1,29 @@
 #!/bin/sh
 
-export DEST=/tmp/HOME
+THIS="$0"
+DEST="$1"
+
+[[ -z $DEST ]] && DEST=$HOME
+
+echo " ** target dir is $DEST"
+
+if [ -L "$THIS" ]; then
+  THIS=`readlink "$THIS"`
+fi
+
+DOTFILES_DIR=`dirname "$THIS"`
+
+pushd $DOTFILES_DIR/tapes
 
 function restore_config_for {
   echo " => restore config for: $1"
-  pushd tapes
   stow "$1" -t $DEST -v
-  popd
   echo " => done."
   echo ""
 }
 
 [[ -d $DEST ]] || mkdir $DEST -p
 
-restore_config_for awesome
 restore_config_for Code
 restore_config_for fontconfig
 restore_config_for htop
@@ -21,10 +31,11 @@ restore_config_for tilix
 restore_config_for zsh
 restore_config_for tmux
 
-[[ -d tapes/nvim ]] || mkdir tapes/vim -p
-[[ -d tapes/nvim/.vim ]] || (
+# we're inside $DOTFILE_DIR/tapes
+[[ -d nvim ]] || mkdir vim -p
+[[ -d nvim/.vim ]] || (
   echo " => downloading vim config";
-  git clone https://git.coding.net/ghlin/vimrc.git tapes/nvim/.vim
+  git clone https://git.coding.net/ghlin/vimrc.git nvim/.vim
 )
 
 restore_config_for nvim
